7b93d5a0ba2253757f7264975d49e4d67a91b909
detectiveseo <detectiveseo1@gmail.com>
2023-03-07 16:26:27 +0600

    added modal for every grid

--- script/function.js
+++ script/function.js
@@ -4,7 +4,9 @@ const productsLoad = async(textFiled) => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${textFiled}`
     const respons = await fetch(url);
     const resData = await respons.json();
+    loadingPlaceholder(true)
     displayData(resData);
+    loadingPlaceholder(false)
 }
 
 const searchProssece = () => {
@@ -15,6 +17,7 @@ const searchProssece = () => {
 
 const displayData = (datas) => {
     const gridWarper = document.getElementById('product-warper');
+    gridWarper.innerHTML = " ";
     datas.data.forEach( singleData => {
         const grid = document.createElement('div');
         grid.classList.add("col-4");
@@ -24,13 +27,40 @@ const displayData = (datas) => {
             <div>
             <h3>${singleData.phone_name}</h3>
             <p>${singleData.brand}</p>
-            <button onclick="modalOpen('${singleData.slug}')" class="bg-primary text-white rounded p-2">See More Detials</button>
+                <button onclick="openModal('${singleData.slug}')" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Launch demo modal
+                </button>
             </div>
         </div>`;
         gridWarper.appendChild(grid);
     });
 }
 
-const modalOpen = (phone) => {
-    console.log(phone);
+const openModal = async (phone) => {
+    const url = `https://openapi.programming-hero.com/api/phone/${phone}`;
+    const respons = await fetch(url);
+    const resJson = await respons.json();
+    const data = resJson.data;
+    modalDiv(data);
+}
+
+const modalDiv = (data) => {
+
+    const title = data.name;
+    const brand = data.brand;
+    const releaseDate = data.releaseDate;
+    document.getElementById('modal_title').innerText = title;
+    document.getElementById('modal_phone_brand').innerText = brand;
+    document.getElementById('modal_phone_born_date').innerText = releaseDate;
+} 
+
+const loadingPlaceholder = (forCase) => {
+    const loader = document.getElementById('loading_placeholder');
+
+    if(forCase === true){
+        loader.classList.remove('d-none')
+    }
+    else{
+        loader.classList.add('d-none')
+    }
 }
